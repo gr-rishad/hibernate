4f1862f62efebe6bfc46d86251f9ec9f8b269e59
Golam Rabbani Rishad <33056647+gr-rishad@users.noreply.github.com>
2022-01-28 18:56:44 +0600

    OneToMany Unideirection completed

---
+++ src/com/luv2code/hibernate/demo/OneToManyUni/GetCourseAndReviewDemo.java
@@ -0,0 +1,54 @@
+package com.luv2code.hibernate.demo.OneToManyUni;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.OneToMany.Course;
+import com.luv2code.hibernate.demo.OneToMany.Review;
+import com.luv2code.hibernate.demo.oneToOne.entity.Instructor;
+import com.luv2code.hibernate.demo.oneToOne.entity.InstructorDetail;
+
+public class GetCourseAndReviewDemo {
+	
+	public static void main(String[] args) {
+		
+		SessionFactory factory= new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class)
+				.buildSessionFactory();
+		
+		// create session
+		Session session= factory.getCurrentSession();
+		
+		try {
+		
+			
+			// start session
+			session.beginTransaction();
+			
+			//get the course
+			int theId= 10;
+			Course tempCourse= session.get(Course.class, theId);
+			
+			//print the course
+			System.out.println("Course Name :"+tempCourse);
+			
+			//print the course reviews
+			System.out.println("Course Reviews :"+tempCourse.getReviews());
+			//commit session
+			session.getTransaction().commit();
+			
+			System.out.println("DONE");
+		}finally {
+			
+			// add clean up code
+			session.close();
+			factory.close();
+		}
+	}
+
+}
